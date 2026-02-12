5acbf76c09028c7e6c1f7567a2d49dea58bcd0c7
hcruz <hum.alex93@gmail>
2026-02-12 14:24:29 -0600

    Create Navbar component

--- blog-front/src/App.vue
+++ blog-front/src/App.vue
@@ -1,15 +1,17 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-  <button class="btn btn-primary">
-  <i class="bi bi-send"></i>
-    Enviar
-  </button>
+  <div>
+    <Navbar />
+    <h2>Bienvenido a mi página</h2>
+  </div>
 </template>
 
-<style scoped></style>
+<script>
+import Navbar from "./components/layout/Navbar.vue"
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
---
+++ blog-front/src/components/layout/Navbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary px-4">
+    
+    <!-- Logo -->
+    <a class="navbar-brand fw-bold" href="#">
+      tltk_art
+    </a>
+
+    <!-- Botón responsive -->
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarMenu"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Links -->
+    <div class="collapse navbar-collapse" id="navbarMenu">
+      
+      <!-- Centro -->
+      <ul class="navbar-nav mx-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Products</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Resources</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Plans & Pricing</a>
+        </li>
+      </ul>
+
+      <!-- Derecha -->
+      <div class="d-flex">
+        <a class="nav-link text-white me-3" href="#">Log In</a>
+        <button class="btn btn-outline-light">
+          Get Started
+        </button>
+      </div>
+    </div>
+  </nav>
+</template>
